a0222c7c6848f626e68f86e422050a2e5c30576a
Filipe Augusto <filipea133@gmail.com>
2016-10-17 01:34:01 -0200

    Imprimindo posicoes possiveis e implementando as restrições de movimento e outros

--- xadrez-console/tabuleiro/Posicao.cs
+++ xadrez-console/tabuleiro/Posicao.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace tabuleiro
+﻿namespace tabuleiro
 {
     class Posicao
     {
         public int linha { get; set; }
         public int coluna { get; set; }
 
-           public Posicao(int linha, int coluna)
+        public Posicao(int linha, int coluna)
+        {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+
+        public void definirValores(int linha, int coluna)
         {
             this.linha = linha;
             this.coluna = coluna;
@@ -20,7 +20,7 @@ namespace tabuleiro
         public override string ToString()
         {
             return linha
-                + " , "
+                + ", "
                 + coluna;
         }
 
